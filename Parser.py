66ce5ede45820e9f23fa99ee091183cfe031dec5
nico-rodriguez <marconicolasrodriguez@gmail.com>
2019-03-24 21:04:22 -0300

    Removed commented code.

--- Parser.py
+++ Parser.py
@@ -30,28 +30,6 @@ def parse_data(file_route, amount_attributes, classes):
         lines_of_split_data.append(split_line)
     print('Finished parsing dataset file')
     return lines_of_split_data
-    '''
-    profits = []
-    lines_of_split_data = lines_of_split_data[0:len(lines_of_split_data)-1]
-    for atribute in range(0, amount_attributes):
-        class_id = lines_of_split_data[0][amount_attributes]
-        for idx, split_line in enumerate(lines_of_split_data, start=0):
-            print("parsing atribute " + str(atribute) + " in line " + str(idx))
-            if (class_id != split_line[amount_attributes]):
-                breakpoint_profit = Utils.profit(
-                    lines_of_split_data, atribute, [split_line[atribute]],
-                    classes)
-                profits.append((breakpoint_profit, split_line[atribute]))
-                class_id = split_line[amount_attributes]
-        ideal_breakpoint = max(profits)
-        for split_line in lines_of_split_data:
-            if (float(split_line[atribute]) <= float(ideal_breakpoint[1])):
-                split_line[atribute] = 0
-            else:
-                split_line[atribute] = 1
-        profits = []
-    return lines_of_split_data
-    '''
 
 
 def process_binary(lines_of_split_data):
